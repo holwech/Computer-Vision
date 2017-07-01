18b598acc35852d8cc7d05f8da599d97ffc42508
Regine1 <regine.hartwig@gmail.com>
2017-07-01 20:17:23 +0200

    fdh

--- HA3_R/F_ransac.m
+++ HA3_R/F_ransac.m
@@ -1,5 +1,20 @@
 function [Korrespondenzen_robust] = F_ransac(Korrespondenzen,varargin)
 % Diese Funktion implementiert den RANSAC-Algorithmus zur Bestimmung von
 % robusten Korrespondenzpunktpaaren
+epsilon=50;
+p=90;
+tolerance=100;
+if(nargin>1)
+    epsilon=varargin{1};
+    if(nargin>2)
+        p=varargin{2};
+        if(nargin>3)
+            tolerance=varargin{3};
+        end
+    end
+end
+k=8;
+s = log(1-p)/log(1-(1-epsilon)^k);
 
+end
 end
--- HA3_R/achtpunktalgorithmus.m
+++ HA3_R/achtpunktalgorithmus.m
@@ -3,7 +3,7 @@ function [EF] = achtpunktalgorithmus(Korrespondenzen,varargin)
 % mittels 8-Punkt-Algorithmus, je nachdem, ob die Kalibrierungsmatrix 'K'
 % vorliegt oder nicht
 cali=0;
-if(nargin==1)
+if(nargin==2)
     K=varargin{1};
     cali=1;
 end
@@ -20,12 +20,13 @@ G = [V_A(1:3,9),V_A(4:6,9),V_A(7:9,9)];
 if(cali==0)
     % estimate E
     S=[1,0,0;0,1,0;0,0,0];
+    fprintf('\n estimate E\n');
     EF = U_G*S*V_G;
-    % 3D-Reconstruction
-    R = 
+    % 3D-Reconstruction? 
 else
     % estimate F
     S_G(3,3)=0;
+    fprintf('\n estimate F\n');
     EF = U_G*S_G*V_G;
 end
 
